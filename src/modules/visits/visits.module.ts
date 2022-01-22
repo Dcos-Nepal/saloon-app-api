703adaa34fc1a46ce5792b3322f7580c92d7abba
Buddha Man Nepali <bmnepali980@gmail.com>
2022-01-23 01:12:04 +0545

    OCA: Fixes imports return type

--- src/modules/visits/visits.module.ts
+++ src/modules/visits/visits.module.ts
@@ -3,7 +3,7 @@ import { VisitsService } from './visits.service';
 import { VisitSchema } from './schemas/visits.schema';
 import { VisitsController } from './visits.controller';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
+import { LoggerMiddleware } from 'src/common/middlewares/middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Visits', schema: VisitSchema }])],
