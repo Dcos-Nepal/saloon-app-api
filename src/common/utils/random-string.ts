9784cd40b7b537be170c52162f0add7737c4e856
Buddha Man Nepali <bmnepali980@gmail.com>
2022-06-01 19:46:26 +0545

    OCA: Updates function to generate random numbers

--- src/common/utils/random-string.ts
+++ src/common/utils/random-string.ts
@@ -1,10 +1,12 @@
 export const randomString = (length = 60) => {
   let output = '';
 
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters =
+    'SlLuU1vmlimJweMYuIvCaVpEFD0o9Zg8Er8TMBsDHfGDtMql13GR6vjEnkxsW8DFMb1NPOv6OzSyr4GaPZDRQcPwndFIDtcSEXTOcz5FC8aLW5vQ11tZxQZzQ1L7WZ0jdT2mBgAPG8o9Hn8jnxWDjrlxkOs06GUB0yxqwq0CMt0SBwVehVVSnGyoe6TH1oPsdEkkQ5WsMsPvpdBVbNUlkqd3K6uYFSG0wrsM6dcxK9NzTcwpWVokMaLiOcSQ8sOE1abATryopa69U4UACW0VeQetiQVgDBaTGRxYuYxo7Hqdcs6RnJvUDPljp7fAFZdH';
 
   for (let i = 0; i < length; i++) {
-    output += characters[Math.floor(Math.random() * length)];
+    const randomNumber = Math.floor(Math.random() * characters.length);
+    output += characters.substring(randomNumber, randomNumber + 1);
   }
 
   return output;
