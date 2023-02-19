bbaf464158a33d9c281e89b2e9f9cae38f537f64
Buddha Man Nepali <bmnepali980@gmail.com>
2023-02-19 07:30:19 +0545

    fix: lint issues

--- src/modules/package-clients/schemas/package-client.schema.ts
+++ src/modules/package-clients/schemas/package-client.schema.ts
@@ -5,7 +5,7 @@ export const PackageClientSchema: any = new Schema(
     customer: { type: Types.ObjectId, ref: 'Customer', required: false, default: null },
     paymentType: { type: String, default: 'CASH' },
     packagePaidDate: { type: Date, required: true, default: Date.now() },
-    noOfSessions: { type: Number, required: true, default: 0},
+    noOfSessions: { type: Number, required: true, default: 0 },
     description: { type: String, required: false },
     isApproved: { type: Boolean, default: false },
     isDeleted: { type: Boolean, default: false }
